bf35d7db211311d5a7dd4c250aa15589c3b30e1a
Zasuj <ale.goma92@gmail.com>
2024-06-22 19:48:12 +0200

    first commit

---
+++ 00_helloworld.py
@@ -0,0 +1,18 @@
+# Esto es un comentario
+# Nuestro hola mundo en python
+print ('Hola python')
+
+"""
+ESte es un comentario entre dobles comillas
+"""
+
+'''
+Este es un comentario entre comillas simples
+'''
+
+#Consultar el tipo de dato:
+print(type("hola mundo")) # hola mundo es el tipo "str" que es una cadena de texto
+print(type(7)) # "7" es un tipo de dato "int" que son numeros enteros
+print(type(1.33)) #"1.33" es un tipo de dato "float" que no son numeros enteros
+print(type(True)) # "True" es un tipo de dato "bool"
+print(type(1 + 3j)) # "1 + 3j" es un tipo de dato "complex" (Complejo) siempre tiene que ser la letra j
